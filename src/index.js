11bd54b9af48ee5f37e18ab5eba8ac10856f76c4
Jan Sudczak <jansudczak@gmail.com>
2019-10-23 22:06:08 +0200

    Remove unused views

--- src/index.js
+++ src/index.js
@@ -6,21 +6,25 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+import Main from "views/Main/Main.js";
+// import LandingPage from "views/LandingPage/LandingPage.js";
+// import ProfilePage from "views/ProfilePage/ProfilePage.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
+
+import UserInfoContext from 'contexts/UserInfoContext/UserInfo.context'
 
 var hist = createBrowserHistory();
 
 ReactDOM.render(
-  <Router history={hist}>
-    <Switch>
-      <Route path="/landing-page" component={LandingPage} />
-      <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
-      <Route path="/" component={Components} />
-    </Switch>
-  </Router>,
+  <UserInfoContext.Provider value={null}>
+    <Router history={hist}>
+      <Switch>
+        {/* <Route path="/landing-page" component={LandingPage} />
+        <Route path="/profile-page" component={ProfilePage} />
+        <Route path="/login-page" component={LoginPage} /> */}
+        <Route path="/" component={Main} />
+      </Switch>
+    </Router>
+  </UserInfoContext.Provider>,
   document.getElementById("root")
 );
